4f9075eb00efcf6867c02e67a90b0e7261fb5a65
Alexgoon <alex.russkovdx@gmail.com>
2017-03-10 17:51:40 +0300

    vb update

--- VB/TilesEx/MainPage.xaml.vb
+++ VB/TilesEx/MainPage.xaml.vb
@@ -1,140 +1,117 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel
-Imports System.IO
-Imports System.Linq
-Imports System.Runtime.InteropServices.WindowsRuntime
+﻿' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
 Imports System.Xml.Serialization
-Imports Windows.Foundation
-Imports Windows.Foundation.Collections
 Imports Windows.Storage
 Imports Windows.Storage.Streams
-Imports Windows.UI.Xaml
-Imports Windows.UI.Xaml.Controls
-Imports Windows.UI.Xaml.Controls.Primitives
-Imports Windows.UI.Xaml.Data
-Imports Windows.UI.Xaml.Input
-Imports Windows.UI.Xaml.Media
-Imports Windows.UI.Xaml.Media.Imaging
-Imports Windows.UI.Xaml.Navigation
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
 
-' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+End Class
 
-Namespace TilesEx
-	''' <summary>
-	''' An empty page that can be used on its own or navigated to within a Frame.
-	''' </summary>
-	Public NotInheritable Partial Class MainPage
-		Inherits Page
+<XmlRoot("Employees")>
+Public Class Employees
+    Inherits List(Of Employee)
 
-		Public Sub New()
-			Me.InitializeComponent()
+End Class
+<XmlRoot("Employee")>
+Public Class Employee
+    Implements INotifyPropertyChanged
 
-		End Sub
-	End Class
+    Public Property Id() As Integer
+    Public Property ParentId() As Integer
+    Public Property FirstName() As String
+    Public Property LastName() As String
+    Public Property JobTitle() As String
+    Public Property Phone() As String
+    Public Property EmailAddress() As String
+    Public Property AddressLine1() As String
+    Public Property City() As String
+    Public Property PostalCode() As String
+    Public Property CountryRegionName() As String
+    Public Property GroupName() As String
+    Public Property BirthDate() As Date
+    Public Property HireDate() As Date
+    Public Property Gender() As String
+    Public Property MaritalStatus() As String
+    Public Property Title() As String
+    Public Property ImageData() As Byte()
 
-	<XmlRoot("Employees")>
-	Public Class Employees
-		Inherits List(Of Employee)
+    Private imageSource_Renamed As BitmapImage
+    <XmlIgnore>
+    Public ReadOnly Property ImageSource() As BitmapImage
+        Get
+            If imageSource_Renamed Is Nothing AndAlso ImageData IsNot Nothing Then
+                SetImageSource()
+            End If
+            Return imageSource_Renamed
+        End Get
+    End Property
+    Private Async Sub SetImageSource()
+        Dim stream As New InMemoryRandomAccessStream()
+        Await stream.WriteAsync(ImageData.AsBuffer())
+        stream.Seek(0)
 
-	End Class
-	<XmlRoot("Employee")>
-	Public Class Employee
-		Implements INotifyPropertyChanged
+        imageSource_Renamed = New BitmapImage()
+        imageSource_Renamed.SetSource(stream)
+        If propertyChanged1 IsNot Nothing Then
+            propertyChanged1(Me, New PropertyChangedEventArgs("ImageSource"))
+        End If
+    End Sub
 
-		Public Property Id() As Integer
-		Public Property ParentId() As Integer
-		Public Property FirstName() As String
-		Public Property LastName() As String
-		Public Property JobTitle() As String
-		Public Property Phone() As String
-		Public Property EmailAddress() As String
-		Public Property AddressLine1() As String
-		Public Property City() As String
-		Public Property PostalCode() As String
-		Public Property CountryRegionName() As String
-		Public Property GroupName() As String
-		Public Property BirthDate() As Date
-		Public Property HireDate() As Date
-		Public Property Gender() As String
-		Public Property MaritalStatus() As String
-		Public Property Title() As String
-		Public Property ImageData() As Byte()
-'INSTANT VB NOTE: The variable imageSource was renamed since Visual Basic does not allow variables and other class members to have the same name:
-		Private imageSource_Renamed As BitmapImage
-		<XmlIgnore>
-		Public ReadOnly Property ImageSource() As BitmapImage
-			Get
-				If imageSource_Renamed Is Nothing AndAlso ImageData IsNot Nothing Then
-					SetImageSource()
-				End If
-				Return imageSource_Renamed
-			End Get
-		End Property
-		Private Async Sub SetImageSource()
-			Dim stream As New InMemoryRandomAccessStream()
-			Await stream.WriteAsync(ImageData.AsBuffer())
-			stream.Seek(0)
+#Region "INotifyPropertyChanged Members"
+    Private propertyChanged1 As PropertyChangedEventHandler
 
-			imageSource_Renamed = New BitmapImage()
-			imageSource_Renamed.SetSource(stream)
-			If propertyChanged IsNot Nothing Then
-				propertyChanged(Me, New PropertyChangedEventArgs("ImageSource"))
-			End If
-		End Sub
+    Private Custom Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+        AddHandler(ByVal value As PropertyChangedEventHandler)
+            propertyChanged1 = DirectCast(System.Delegate.Combine(propertyChanged1, value), PropertyChangedEventHandler)
+        End AddHandler
+        RemoveHandler(ByVal value As PropertyChangedEventHandler)
+            propertyChanged1 = DirectCast(System.Delegate.Remove(propertyChanged1, value), PropertyChangedEventHandler)
+        End RemoveHandler
+        RaiseEvent(ByVal sender As System.Object, ByVal e As System.ComponentModel.PropertyChangedEventArgs)
+            If propertyChanged1 IsNot Nothing Then
+                For Each d As PropertyChangedEventHandler In propertyChanged1.GetInvocationList()
+                    d.Invoke(sender, e)
+                Next d
+            End If
+        End RaiseEvent
+    End Event
+#End Region
+End Class
+Public Class ViewModel
+    Inherits DevExpress.Mvvm.BindableBase
 
-		#Region "INotifyPropertyChanged Members"
-		Private propertyChanged As PropertyChangedEventHandler
+    Public ReadOnly Property Employees() As Employees
+        Get
+            Return DataStorage.Employees
+        End Get
+    End Property
+End Class
+Public NotInheritable Class DataStorage
 
-		Private Custom Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
-			AddHandler(ByVal value As PropertyChangedEventHandler)
-				propertyChanged = DirectCast(System.Delegate.Combine(propertyChanged, value), PropertyChangedEventHandler)
-			End AddHandler
-			RemoveHandler(ByVal value As PropertyChangedEventHandler)
-				propertyChanged = DirectCast(System.Delegate.Remove(propertyChanged, value), PropertyChangedEventHandler)
-			End RemoveHandler
-			RaiseEvent(ByVal sender As System.Object, ByVal e As System.ComponentModel.PropertyChangedEventArgs)
-				If propertyChanged IsNot Nothing Then
-					For Each d As PropertyChangedEventHandler In propertyChanged.GetInvocationList()
-						d.Invoke(sender, e)
-					Next d
-				End If
-			End RaiseEvent
-		End Event
-		#End Region
-	End Class
-	Public Class ViewModel
-		Inherits DevExpress.Mvvm.BindableBase
+    Private Sub New()
+    End Sub
 
-		Public ReadOnly Property Employees() As Employees
-			Get
-				Return DataStorage.Employees
-			End Get
-		End Property
-	End Class
-	Public NotInheritable Class DataStorage
 
-		Private Sub New()
-		End Sub
-
-'INSTANT VB NOTE: The variable employees was renamed since Visual Basic does not allow variables and other class members to have the same name:
-		Private Shared employees_Renamed As Employees
-		Public Shared ReadOnly Property Employees() As Employees
-			Get
-				If employees_Renamed IsNot Nothing Then
-					Return employees_Renamed
-				End If
-				Try
-					Dim file As StorageFile = StorageFile.GetFileFromApplicationUriAsync(New Uri("ms-appx:///Data/Employees.xml")).AsTask().Result
-					Dim stream As Stream = file.OpenStreamForReadAsync().Result
-					Dim serializier As New XmlSerializer(GetType(Employees))
-					employees_Renamed = TryCast(serializier.Deserialize(stream), Employees)
-				Catch
-					employees_Renamed = New Employees()
-					employees_Renamed.Add(New Employee() With {.Id = 109, .FirstName = "Bruce", .LastName = "Cambell", .JobTitle = "Chief Executive Officer", .Phone = "(417) 166-3268", .EmailAddress = "Bruce_Cambell@example.com", .AddressLine1 = "4228 S National Ave", .City = "Tokyo", .PostalCode = "65809", .CountryRegionName = "Japan", .GroupName = "Executive General and Administration", .Gender = "M", .Title = "Mr."})
-				End Try
-				Return employees_Renamed
-			End Get
-		End Property
-	End Class
-End Namespace
+    Private Shared employees_Renamed As Employees
+    Public Shared ReadOnly Property Employees() As Employees
+        Get
+            If employees_Renamed IsNot Nothing Then
+                Return employees_Renamed
+            End If
+            Try
+                Dim file As StorageFile = StorageFile.GetFileFromApplicationUriAsync(New Uri("ms-appx:///Data/Employees.xml")).AsTask().Result
+                Dim stream As Stream = file.OpenStreamForReadAsync().Result
+                Dim serializier As New XmlSerializer(GetType(Employees))
+                employees_Renamed = TryCast(serializier.Deserialize(stream), Employees)
+            Catch
+                employees_Renamed = New Employees()
+                employees_Renamed.Add(New Employee() With {.Id = 109, .FirstName = "Bruce", .LastName = "Cambell", .JobTitle = "Chief Executive Officer", .Phone = "(417) 166-3268", .EmailAddress = "Bruce_Cambell@example.com", .AddressLine1 = "4228 S National Ave", .City = "Tokyo", .PostalCode = "65809", .CountryRegionName = "Japan", .GroupName = "Executive General and Administration", .Gender = "M", .Title = "Mr."})
+            End Try
+            Return employees_Renamed
+        End Get
+    End Property
+End Class
--- VB/TilesEx/My Project/AssemblyInfo.vb
+++ VB/TilesEx/My Project/AssemblyInfo.vb
@@ -1,18 +1,19 @@
-﻿Imports System.Reflection
-Imports System.Runtime.CompilerServices
+﻿Imports System
+Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
+
+' Review the values of the assembly attributes
+
 <Assembly: AssemblyTitle("TilesEx")>
 <Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("TilesEx")>
 <Assembly: AssemblyCopyright("Copyright ©  2016")>
 <Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -23,7 +24,8 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
-' [assembly: AssemblyVersion("1.0.*")]
+' <Assembly: AssemblyVersion("1.0.*")> 
+
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
 <Assembly: ComVisible(False)>
